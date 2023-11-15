892932589aae323d4bd55b2201d9e6cf8dd24026
Igor Pereira <pereira.t.igor@gmail.com>
2023-11-15 10:25:47 -0300

    Started react basics project

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+// const user = {  }
+
+// //Optional Chaining
+// console.log(user?.address?.street);
+
+ReactDOM.render(
+  <h1 id="greetings">
+  <span>Olá, Mundo!</span>
+  </h1>, 
+  document.getElementById('root'),
+);
